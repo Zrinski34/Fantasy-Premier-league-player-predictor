f3b89b09b7fd65cf2319a36ad6dd91ffaafb82c5
Zrinski34 <zrinski34@gmail.com>
2022-02-27 10:47:49 +0100

    My first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests
+URL='https://fantasy.premierleague.com/api/bootstrap-static/'
+def get_player_data():
+    response = requests.get(URL)
+    data = response.json()
+    print(data)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    get_player_data()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

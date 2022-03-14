c52cad5e607e7481a7b5dac0f77d9c02d8897346
MatijaDomjan <matijadomjan@gmail.com>
2022-03-14 15:41:19 +0100

    matijas's first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import pandas
 URL='https://fantasy.premierleague.com/api/bootstrap-static/'
 def get_player_data():
     response = requests.get(URL)
@@ -6,6 +7,5 @@ def get_player_data():
     print(data)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     get_player_data()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
